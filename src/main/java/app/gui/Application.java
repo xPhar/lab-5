e612a3e19871254ecf6aee0dd358088385c630f1
Wuqingyi Wang <wuqingyiwang@gmail.com>
2025-10-07 12:23:04 -0400

    Update Application.java

--- src/main/java/app/gui/Application.java
+++ src/main/java/app/gui/Application.java
@@ -362,7 +362,7 @@ public class Application {
 
                 try {
                     final float top = getTopGradeUseCase.getTopGrade(course);
-                    JOptionPane.showMessageDialog(jFrame, "Average Grade: " + top);
+                    JOptionPane.showMessageDialog(jFrame, "Top Grade: " + top);
                     courseField.setText("");
                 }
                 catch (JSONException ex) {
